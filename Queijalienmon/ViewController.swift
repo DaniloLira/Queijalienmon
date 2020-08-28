9ceeddf98aef13cc74a01536b246644f712306b9
Danilo Lira <danilo.lira01@gmail.com>
2020-08-28 10:03:08 -0300

    Initial Commit

---
+++ Queijalienmon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Queijalienmon
+//
+//  Created by Danilo Araújo on 28/08/20.
+//  Copyright © 2020 mandabrasa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
